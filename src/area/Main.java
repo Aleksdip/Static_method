2d0a7fee6d5d498857c1356d5b9c71bd69289a06
Aleksdip <aleks.viunoff@gmail.com>
2022-11-30 20:26:51 +0200

    Використання static

---
+++ src/area/Main.java
@@ -0,0 +1,7 @@
+package area;
+
+public class Main {
+    public static void main(String[] args) {
+        MyArea.areaOfCircle(5);
+    }
+}
---
+++ src/area/MyArea.java
@@ -0,0 +1,9 @@
+package area;
+
+public class MyArea {
+    final static double PI = 3.14;
+
+    static void areaOfCircle (double radius) {
+        System.out.println(Math.pow(radius, 2) * PI);
+    }
+}
